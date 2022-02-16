504dbed7cbd34f805117b6adb1ca79b318c441f5
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-02-16 11:18:04 +0900

    login 화면 구현

--- app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/login/LoginFragment.kt
@@ -1,2 +1,8 @@
 package com.kdw.mystorymedia.ui.auth.fragment.login
 
+import androidx.fragment.app.Fragment
+import com.kdw.mystorymedia.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
--- app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/signup/SignUpFragment.kt
+++ app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/signup/SignUpFragment.kt
@@ -1,2 +1,8 @@
 package com.kdw.mystorymedia.ui.auth.fragment.signup
 
+import androidx.fragment.app.Fragment
+import com.kdw.mystorymedia.R
+
+class SignUpFragment : Fragment(R.layout.fragment_register) {
+
+}
