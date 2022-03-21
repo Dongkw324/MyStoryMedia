befd3466601ab8813d5ad19fe99a29009d05aff9
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-03-21 10:31:38 +0900

    Event wrapper

---
+++ app/src/main/java/com/kdw/mystorymedia/util/Event.kt
@@ -0,0 +1,29 @@
+package com.kdw.mystorymedia.util
+
+import androidx.lifecycle.Observer
+
+class Event<out T>(private val content: T) {
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled() : T? {
+        return if(!hasBeenHandled) {
+            hasBeenHandled = true
+            content
+        } else null
+    }
+
+    fun peekContent() = content
+}
+
+class EventObserver<T>(
+    private inline val onError: ((String) -> Unit) ? = null,
+    private inline val onLoading: (() -> Unit)? = null,
+    private inline val onSuccess: (T) -> Unit
+) : Observer<Event<Resource<T>>> {
+
+    override fun onChanged(t: Event<Resource<T>>?) {
+        
+    }
+
+}
