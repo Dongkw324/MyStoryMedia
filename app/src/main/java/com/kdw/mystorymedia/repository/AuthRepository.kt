4e0d39ed34dd734c1ecc6de8a2e70a8bac6112ae
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-03-25 20:24:33 +0900

    AuthRepository

---
+++ app/src/main/java/com/kdw/mystorymedia/repository/AuthRepository.kt
@@ -0,0 +1,11 @@
+package com.kdw.mystorymedia.repository
+
+import com.google.firebase.auth.AuthResult
+import com.kdw.mystorymedia.util.Resource
+
+interface AuthRepository {
+
+    suspend fun login(email: String, password: String) : Resource<AuthResult>
+    suspend fun register(userName: String, email: String, password: String) : Resource<AuthResult>
+    suspend fun resetPwd(email: String) : Resource<AuthResult>
+}
