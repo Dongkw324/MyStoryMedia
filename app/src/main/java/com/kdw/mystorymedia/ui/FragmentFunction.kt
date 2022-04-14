faceee6b1843d517073fb86bb6275c612265ad5f
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-04-14 10:00:46 +0900

    snackbar extension function

---
+++ app/src/main/java/com/kdw/mystorymedia/ui/FragmentFunction.kt
@@ -0,0 +1,12 @@
+package com.kdw.mystorymedia.ui
+
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+
+fun Fragment.snackBar(text: String) {
+    Snackbar.make(
+        requireView(),
+        text,
+        Snackbar.LENGTH_SHORT
+    ).show()
+}
