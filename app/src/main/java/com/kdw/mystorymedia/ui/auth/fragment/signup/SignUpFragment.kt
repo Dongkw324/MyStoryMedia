3ddb2a6260ff62574b787433b2ad42a366cfea17
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-04-20 21:28:57 +0900

    SignUpFragment

--- app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/signup/SignUpFragment.kt
+++ app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/signup/SignUpFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.kdw.mystorymedia.R
 import com.kdw.mystorymedia.databinding.FragmentRegisterBinding
 import com.kdw.mystorymedia.ui.auth.viewModel.AuthViewModel
@@ -20,5 +22,16 @@ class SignUpFragment : Fragment(R.layout.fragment_register) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentRegisterBinding.bind(view)
+        authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+
+        binding.apply {
+            loginTxt.setOnClickListener {
+                if(findNavController().previousBackStackEntry != null) {
+                    findNavController().popBackStack()
+                } else {
+                    SignUpFragmentDirections.actionSignUpFragmentToLoginFragment()
+                }
+            }
+        }
     }
 }
