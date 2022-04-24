24131fdf6e60f97881fc046d671bb644906fc751
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2022-04-24 18:58:50 +0900

    LoginFragment

--- app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/kdw/mystorymedia/ui/auth/fragment/login/LoginFragment.kt
@@ -1,8 +1,39 @@
 package com.kdw.mystorymedia.ui.auth.fragment.login
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.kdw.mystorymedia.R
+import com.kdw.mystorymedia.databinding.FragmentLoginBinding
+import com.kdw.mystorymedia.ui.auth.viewModel.AuthViewModel
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var authViewModel: AuthViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentLoginBinding.bind(view)
+        authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+
+        binding.apply {
+            resetTextBtn.setOnClickListener {
+                findNavController().navigate(
+                    LoginFragmentDirections.actionLoginFragmentToPwdResetFragment()
+                )
+            }
+
+            registerTextBtn.setOnClickListener {
+                if(findNavController().previousBackStackEntry != null) {
+                    findNavController().popBackStack()
+                } else {
+                    LoginFragmentDirections.actionLoginFragmentToSignUpFragment()
+                }
+            }
+        }
+    }
 }
